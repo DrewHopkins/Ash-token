f4537811ffc51cad7595427f41ea340370625e5d
Drew Hopkins <drewhopkins@Drews-MBP.attlocal.net>
2021-12-24 22:52:31 -0500

    Deployed Contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
-var DappToken = artifacts.require("./DappToken.sol");
-var DappTokenSale = artifacts.require("./DappTokenSale.sol");
+var AshToken = artifacts.require("./AshToken.sol");
+var AshTokenSale = artifacts.require("./AshTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000).then(function() {
+  deployer.deploy(AshToken, 1000000).then(function() {
     // Token price is 0.001 Ether
     var tokenPrice = 1000000000000000;
-    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+    return deployer.deploy(AshTokenSale, AshToken.address, tokenPrice);
   });
 };
